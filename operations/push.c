923dc74a70d765b7df6b75d1c79b145d28f4fd4a
lavendelchen <lavendel02sh@gmail.com>
2022-02-15 21:19:45 +0100

    little try that pretty much failed ahahahahahh

--- operations/push.c
+++ operations/push.c
@@ -6,7 +6,7 @@
 /*   By: shaas <shaas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 20:26:43 by shaas             #+#    #+#             */
-/*   Updated: 2022/02/15 21:13:45 by shaas            ###   ########.fr       */
+/*   Updated: 2022/02/15 21:19:28 by shaas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,15 @@ void	push_b(t_list *stack_a, t_list *stack_b)
 	also what if there's nothing in a lol
 	
 	push = stack_a->start;
-	if (stack_b->start == NULL)
+	if (stack_b->start == NULL) // this is wrong so far
+		stack_b->end = push;
+	else
 	{
-		ps_lstinit(stack_b, )
+		push->next = stack_b->start;
+		stack_b->start->prev = push;
 	}
 	stack_a->start = stack_a->start->next;
 	stack_a->start->prev = NULL;
-	push->next = stack_b->start;
-	stack_b->start->prev = push;
 	stack_b->start = push;
 }
 
